f2d715cf48e7b43aeab28a5919a8e83d9641a3d3
Lujza Milotova <milotovl@natur.cuni.cz>
2024-02-24 09:36:09 +0100

    Update QuizPrep class

--- app/src/main/java/org/lujza/QuizPrep.java
+++ app/src/main/java/org/lujza/QuizPrep.java
@@ -16,24 +16,31 @@ public class QuizPrep {
         mode = getUserChoice(scanner, 2);
 
         System.out.println("Now choose a topic you would like to study.");
-        System.out.println("Enter 1 for Networking Acronyms");
-        System.out.println("Enter 2 for Bioinformatic Acronyms");
-        System.out.println("Enter 3 for Populations of European Countries");
+        System.out.println("Enter 1 for Bioinformatic Acronyms");
+        System.out.println("Enter 2 for Morse Code");
+        System.out.println("Enter 3 for Networking Acronyms");
+        System.out.println("Enter 4 for Bioinformatic Algorithms");
+        System.out.println("Enter 5 for Latin Phrases");
+        System.out.println("Enter 6 for TODO");
+        System.out.println("Enter 7 for TODO");
+        System.out.println("Enter 8 for TODO");
+        System.out.println("Enter 9 for TODO");
+
         topic = getUserChoice(scanner, 3);
 
         // triedy TextAnswer, SingleChoice a Multiple choice by obsluhovali urcity typ testu,
         // volali by sa s konkretnym suborom (cestou k nemu) a modom studia (precvicovanie / testovanie)
 
         switch (topic) {
-            case 1 -> TextAnswer(bioinf.json, mode);
-            case 2 -> TextAnswer(morse.json, mode);
-            case 3 -> TextAnswer(networking.json, mode);
-            case 4 -> SingleChoice(bioinf_alg.json, mode);
-            case 5 -> SingleChoice(latin.json, mode);
-            case 6 -> SingleChoice(TODO, mode);
-            case 7 -> MultipleChoice(TODO, mode);
-            case 8 -> MultipleChoice(TODO, mode);
-            case 9 -> MultipleChoice(TODO, mode);
+            case 1 -> TextAnswer(data/text/bioinf.json, mode);
+            case 2 -> TextAnswer(data/text/morse.json, mode);
+            case 3 -> TextAnswer(data/text/networking.json, mode);
+            case 4 -> SingleChoice(data/single-choice/bioinf_alg.json, mode);
+            case 5 -> SingleChoice(data/single-choice/latin.json, mode);
+            case 6 -> SingleChoice(data/single-choice/TODO.json, mode);
+            case 7 -> MultipleChoice(data/multiple-choice/TODO.json, mode);
+            case 8 -> MultipleChoice(data/multiple-choice/TODO.json, mode);
+            case 9 -> MultipleChoice(data/multiple-choice/TODO.json, mode);
         }
     }
 
